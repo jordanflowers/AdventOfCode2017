9281e1533fc01b5fb32b6aac01b781f4b69107d3
Jordan Flowers <jordanflowers00@gmail.com>
2017-12-10 12:21:23 -0500

    day 2 part 1 done

--- 2/part1.py
+++ 2/part1.py
@@ -1,18 +1,30 @@
 with open('input.txt') as f:
-        min = 0
-        max = 0
-        firstIteration = 1
+        lst = []
         for line in f:
+                lst.append(line.split())
+        i = 0
+        sum = 0
+        for item in lst:
+                min = 0
+                max = 0
+                temp = 0
+                
                 firstIteration = 1
-                for number in line:
-                        print(number)
-                        #if firstIteration == 1:
-                        #        min = int(number)
-                        #        max = int(number)
-                        #        firstIteration = 0
-                        #else:
-                        #        if min > int(number):
-                        #                min = int(number)
-                        #        elif max < int(number):
-                        #                max = int(number)
-                #print ("max: %d min: %d") % (max, min)
+                for number in item:
+                        temp = 0
+                        if firstIteration == 1:
+                                min = int(number)
+                                max = int(number)
+                                firstIteration = 0
+                        else:
+                                #just get the min for now
+                                if int(number) < min:
+                                        min = int(number)
+                                if int(number) > max:
+                                        max = int(number)
+                print("max: ", max, "min: ", min)
+                print("temp is now equal to ", max-min)
+                temp = max - min
+                sum = sum + temp
+                
+        print(sum)
